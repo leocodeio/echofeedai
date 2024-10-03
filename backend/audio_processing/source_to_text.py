8300f0cb6ab6c742778f4b952b6b474a6e6195f6
leodoeio <basicleo787@gmail.com>
2024-10-03 14:24:50 +0530

    updated source and speech inputs

--- backend/audio_processing/source_to_text.py
+++ backend/audio_processing/source_to_text.py
@@ -1,23 +1,20 @@
 import speech_recognition as sr # type: ignore
 
-
-def source_to_test(audio_file):
+def source_to_text(audio_file):
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_file) as source:
         print("Loading audio file...")
         audio = recognizer.record(source)
 
     try:
-        # Convert the captured audio to text using Google Web Speech API
         print("Recognizing speech...")
         text = recognizer.recognize_google(audio)
-        print("You said:", text)
+        return text
     except sr.UnknownValueError:
         print("Could not understand the audio.")
     except sr.RequestError as e:
         print(f"Could not request results from the service; {e}")
 
-    return text
 
-text = source_to_test('../dataset/OSR_us_000_0010_8k.wav')
+text = source_to_text('../dataset/OSR_us_000_0010_8k.wav')
 print(text)
