0fa0162df5d22a2f50eae3798d8ae7e7e85a67d4
leodoeio <basicleo787@gmail.com>
2024-10-03 14:13:49 +0530

    added speech to text

---
+++ backend/audio_processing/source_to_text.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr # type: ignore
+
+
+def source_to_test(audio_file):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+        print("Loading audio file...")
+        audio = recognizer.record(source)
+
+    try:
+        # Convert the captured audio to text using Google Web Speech API
+        print("Recognizing speech...")
+        text = recognizer.recognize_google(audio)
+        print("You said:", text)
+    except sr.UnknownValueError:
+        print("Could not understand the audio.")
+    except sr.RequestError as e:
+        print(f"Could not request results from the service; {e}")
+
+    return text
+
+text = source_to_test('../dataset/OSR_us_000_0010_8k.wav')
+print(text)
