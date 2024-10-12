a9bbf5590e6297f08c272bdbae23697ff7e0b7e9
leodoeio <basicleo787@gmail.com>
2024-10-12 17:15:15 +0530

    updated code for question generation

---
+++ backend/input_generator/question_generator.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+
+def generate_questions(topics):
+    questions = []
+    for topic in topics:
+        prompt = f"Generate a single feedback question which can be asked to a customer/user/employee to give the response on what he/she thinks regarding,{topic} and keep it understandable and simple with lessthan or equal to 12 words."
+        response = model.generate_content(prompt)
+        questions.append(response.text)
+    return questions
+
+number_of_topics = 5
+topics = ["work environment", "salary", "timings","play time","peer pressure"]
+questions = generate_questions(topics)
+[print(x) for x in questions]
