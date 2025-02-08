ba3af966c47ddf6a552d1204e9b518263101f0d0
leocodeio <basicleo787@gmail.com>
2025-02-08 07:17:31 +0000

    FEAT: Backend and model is connected

--- echofeedai-backend/src/controllers/model.ts
+++ echofeedai-backend/src/controllers/model.ts
@@ -22,16 +22,29 @@ export const generateQuestions = async (req: Request, res: Response) => {
   }
 
   // Call the python model here, and get the response
+  const pythonModelApi = process.env.BACKEND_PYTHON_MODEL_API;
+  const response = await axios.post(`${pythonModelApi}/generate-questions`, {
+    topics: topicRequestData.data.topics,
+  });
+  if (response.status !== 200) {
+    res.status(500).json({
+      message: "Failed to generate questions",
+      payload: {},
+    });
+    return;
+  }
+  // console.log("response", response.data.question_map);
+  const { question_map } = response.data as TopicToQuestionMap;
+  // console.log("topics", question_map);
 
-  const { topics } = {} as TopicToQuestionMap;
-  console.log("topics", topics);
   res.status(200).json({
     message: "Questions generated successfully",
     payload: {
-      topics,
+      question_map,
     },
   });
   return;
+
 };
 
 
@@ -49,7 +62,13 @@ export const getCoverage = async (req: Request, res: Response) => {
   };
 
   // Call the python model here, and get the response
-  const { all_topics_covered, covered, not_covered } = {} as CoverageResult;
+  const pythonModelApi = process.env.BACKEND_PYTHON_MODEL_API;
+  const response = await axios.post(`${pythonModelApi}/process-feedback`, {
+    question_map: coverageRequest.data.question_map,
+    employee_text: coverageRequest.data.employee_text,
+  });
+  const { all_topics_covered, covered, not_covered } = response.data as CoverageResult;
+
   res.status(200).json({
     message: "Coverage fetched successfully",
     payload: {
